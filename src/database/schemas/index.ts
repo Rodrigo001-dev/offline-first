e6f75b7dbe0beacdcde96a8530a54680ed86867d
Rodrigo <rodrigorael53@gmail.com>
2022-06-30 00:11:29 -0300

    change the version to reload the schemas for the db to start without any information

--- src/database/schemas/index.ts
+++ src/database/schemas/index.ts
@@ -5,6 +5,6 @@ import { skillSchema } from './skillSchema';
 export const schemas = appSchema({
   // através da version é possível controlar as atualizações na estrutura do banco
   // se mudar a version vai ser recriado toda a estrutura do banco
-  version: 1,
+  version: 2,
   tables: [ skillSchema ]
 });
